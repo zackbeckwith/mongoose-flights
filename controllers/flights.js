9a1d9da4c93b24c6aadead4c7fe242e60496a1d6
zackbeckwith <z.beckwith99@gmail.com>
2022-03-01 20:40:24 -0500

    add new functionality

---
+++ controllers/flights.js
@@ -0,0 +1,18 @@
+import { Flight } from '../models/flight.js'
+
+function newFlight(req, res) {
+  res.render('flights/new')
+}
+
+function create(req, res) {
+  const flight = new Flight(req.body)
+  flight.save(function(err) {
+    if (err) return res.redirect('/flights/new')
+    res.redirect('/movies/new')    
+  })
+}
+export {
+  newFlight as new,
+  create,
+
+}
---
+++ routes/flights.js
@@ -0,0 +1,13 @@
+import { Router } from 'express'
+const router = Router()
+import * as flightsCtrl from '../controllers/flights.js'
+
+// GET /flights/new
+router.get('/new', flightsCtrl.new)
+
+//POST /flights
+router.post('/', flightsCtrl.create)
+
+export {
+	router
+}
